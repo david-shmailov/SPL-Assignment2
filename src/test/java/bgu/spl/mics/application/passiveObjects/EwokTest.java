6886b8683c90d6ca4a1173d4ab5d11ddb0f96a6e
David Shmailov <shmailov@post.bgu.ac.il>
2020-11-24 16:43:55 +0200

    created ewok test

---
+++ src/test/java/bgu/spl/mics/application/passiveObjects/EwokTest.java
@@ -0,0 +1,26 @@
+package bgu.spl.mics.application.passiveObjects;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EwokTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void acquire() {
+    }
+
+    @Test
+    void release() {
+    }
+}
