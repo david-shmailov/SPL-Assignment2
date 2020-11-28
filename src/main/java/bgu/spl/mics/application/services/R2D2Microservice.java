0221d4cb56ee2f7f107c88e238ac9ef41efff6b1
David Shmailov <shmailov@post.bgu.ac.il>
2020-11-28 20:19:41 +0200

    implemented R2D2

--- src/main/java/bgu/spl/mics/application/services/R2D2Microservice.java
+++ src/main/java/bgu/spl/mics/application/services/R2D2Microservice.java
@@ -1,6 +1,9 @@
 package bgu.spl.mics.application.services;
 
+import bgu.spl.mics.Callback;
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.DeactivationEvent;
+import bgu.spl.mics.application.messages.TerminateBroadcast;
 
 /**
  * R2D2Microservices is in charge of the handling {@link DeactivationEvent}.
@@ -11,13 +14,30 @@ import bgu.spl.mics.MicroService;
  * You MAY change constructor signatures and even add new public constructors.
  */
 public class R2D2Microservice extends MicroService {
+    private long duration;
 
     public R2D2Microservice(long duration) {
         super("R2D2");
+        this.duration=duration;
     }
 
     @Override
     protected void initialize() {
+        Callback<DeactivationEvent> shnatz = new Callback<DeactivationEvent>() {
+            @Override
+            public void call(DeactivationEvent c) throws InterruptedException {
+                Thread.currentThread().sleep(duration);
+                complete(c,true);
+            }
+        };
+        this.subscribeEvent(DeactivationEvent.class,shnatz);
 
+        Callback<TerminateBroadcast> terminate= new Callback<TerminateBroadcast>() {
+            @Override
+            public void call(TerminateBroadcast c) throws InterruptedException {
+                terminate();
+            }
+        };
+        this.subscribeBroadcast(TerminateBroadcast.class,terminate);
     }
 }
