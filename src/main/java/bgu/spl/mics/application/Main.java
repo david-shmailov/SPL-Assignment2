9e63e4618c4d2acbf23f76e0b7fa208606c0b57e
aviramlachmani <aviraml@post.bgu.ac.il>
2020-12-03 15:07:10 +0200

    aviram update

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -1,20 +1,16 @@
 package bgu.spl.mics.application;
 
 import bgu.spl.mics.User;
-import bgu.spl.mics.User2;
 import bgu.spl.mics.application.passiveObjects.Attack;
 import bgu.spl.mics.application.passiveObjects.Diary;
 import bgu.spl.mics.application.passiveObjects.Ewoks;
 import bgu.spl.mics.application.services.*;
 import com.google.gson.Gson;
 
-import java.util.List;
-import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Vector;
 
 
 /** This is the Main class of the application. You should parse the input file,
@@ -30,38 +26,28 @@ public class Main {
 			Gson gson = new Gson();
 			Reader reader = Files.newBufferedReader(Paths.get(args[0]));
 			User user = gson.fromJson(reader,User.class);
-			User2 user2[] = new User2[user.attacks.length];
+			Attack attacks[] = new Attack[user.attacks.length];
 			for(int i=0; i<user.attacks.length;i++) {
-				user2[i] = gson.fromJson(user.attacks[i], User2.class);
+				attacks[i] = gson.fromJson(user.attacks[i], Attack.class);
 			}
 			reader.close();
-			/*
+			/**
 			this mess first initializes an array of Attacks. then it must go through each user2[i] "Attack"
 			and make a real attack object using the relevant data. to do this it also has to transfer the current array of ints
 			into a List data structure (chosen here to be vector). it then creates an attack object with said vector and the attack length,
 			and pushes that new Attack object into the attacks array we created. this is all so we can initialize leia.
-			TODO try to make this part more efficient , maybe create this attack array somehow straight from the json.
 			 */
-			Attack attacks[] = new Attack[user.attacks.length];
-			for (int i = 0; i < user.attacks.length ; i++){
-				List<Integer> list = new Vector<Integer>();
-				for (int j = 0; j< user2[i].serials.length; j++){
-					list.add(user2[i].serials[j]);
-				}
-				attacks[i] = new Attack(list , user2[i].serials.length);
-			}
-
 
 
 			//Main logic
 			//init objects
+			Diary diary=Diary.getInstance();
 			Ewoks ewokHome = new Ewoks(user.Ewoks);
 			HanSoloMicroservice Han = new HanSoloMicroservice(ewokHome);
 			C3POMicroservice C3PO = new C3POMicroservice(ewokHome);
 			LeiaMicroservice Leia = new LeiaMicroservice(attacks);
 			LandoMicroservice Lando = new LandoMicroservice(user.Lando);
 			R2D2Microservice R2D2 = new R2D2Microservice(user.R2D2);
-			Diary diary=Diary.getInstance();
 
 			//init threads
 			Thread han = new Thread(Han);
@@ -81,6 +67,7 @@ public class Main {
 			leia.join();
 			lando.join();
 			r2d2.join();
+
 
 
 
